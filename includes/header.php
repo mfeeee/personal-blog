83ffccc2c9f3e1c3aff7889fb9627ae16bf7ce6c
mafe <mfernandarocosta@gmail.com>
2026-03-02 16:54:09 -0300

    feat(ui): implement base header layout and assets inclusion

---
+++ includes/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Personal Blog | Maria Fernanda</title>
+    <link href="https://cdn.boxicons.com/3.0.8/fonts/basic/boxicons.min.css" rel="stylesheet"/>
+    <link href="/style.css" rel="stylesheet">
+    <script src="/script.js"></script>
+</head>
+<body>
+    <div class="container" id="main-container">
+        <main class="content-list">
+            <header>
+                <div>
+                    <i class="bx bx-quote-right"></i>
+                </div>
+                <div class="profile">
+                    <h1>Maria Fernanda</h1>
+                    <h2>Software Engineer</h2>
+                </div>
+                <div class="profile-description">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas adipisci, eos obcaecati labore rem expedita ipsum nisi vitae vero odio ducimus veritatis facilis voluptates dolores delectus, nobis cumque velit beatae?</p>
+                </div>
+            </header>
+
+            
