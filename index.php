6912096b10e2c49faee8e72526f4928846d1450d
mafe <mfernandarocosta@gmail.com>
2026-03-04 14:07:31 -0300

    feat(ui): refactor article display to group by year for improved organization

--- index.php
+++ index.php
@@ -3,23 +3,34 @@
 require_once __DIR__. '/includes/functions.php';
 
 $articles = getAllArticles();
+$articlesByYear = [];
+
+foreach ($articles as $article) {
+    $year = date('Y', strtotime($article['date']));
+    $articlesByYear[$year][] = $article;
+}
+
+krsort($articlesByYear);
 
 include __DIR__ . '/includes/header.php';
 ?>
 
-<section class="year-group">
-    <p class="year">2026</p>
-    <?php foreach($articles as $article): ?>
-        <?php 
-            $jsTitle = json_encode($article['title']);
-            $jsContent = json_encode($article['content']);
-        ?>
-        <div class="post-item" onclick='changeContent(<?= $jsTitle ?>, <?= $jsContent ?>)'>
-            <span class="post-date"><?= $article['date'] ?></span>
-            <span class="post-title"><?= $article['title'] ?></span>
-        </div>
-    <?php endforeach ?>
-</section>
+<?php foreach ($articlesByYear as $year => $posts): ?>
+    <section class="year-group">
+        <p class="year"><?= $year ?></p>
+
+        <?php foreach($posts as $article): ?>
+            <?php 
+                $jsTitle = json_encode($article['title']);
+                $jsContent = json_encode($article['content']);
+            ?>
+            <div class="post-item" onclick='changeContent(<?= $jsTitle ?>, <?= $jsContent ?>)'>
+                <span class="post-date"><?= date('M d', strtotime($article['date'])) ?></span>
+                <span class="post-title"><?= $article['title'] ?></span>
+            </div>
+        <?php endforeach ?>
+    </section>
+<?php endforeach ?>
 
 <?php include __DIR__ . '/includes/footer.php' ?>
 
