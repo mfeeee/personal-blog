a91cad520b1b853c5b7ff7740bd9d08697c08bc6
mafe <mfernandarocosta@gmail.com>
2026-03-04 14:39:23 -0300

    feat(ui): add favicon link for improved branding

--- includes/head.php
+++ includes/head.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Personal Blog | Maria Fernanda</title>
+    <link rel="icon" type="image/png" href="/assets/quote-right.png">
     <link href="https://cdn.boxicons.com/3.0.8/fonts/basic/boxicons.min.css" rel="stylesheet"/>
     <link href="/style.css" rel="stylesheet">
     <script src="/script.js"></script>
