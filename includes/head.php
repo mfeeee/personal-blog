fab6d7e9b75a0e81f3e6b5c5694aef64d50b323b
mafe <mfernandarocosta@gmail.com>
2026-03-04 13:56:05 -0300

    feat(ui): add initial HTML structure for the head section

---
+++ includes/head.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Personal Blog | Maria Fernanda</title>
+    <link href="https://cdn.boxicons.com/3.0.8/fonts/basic/boxicons.min.css" rel="stylesheet"/>
+    <link href="/style.css" rel="stylesheet">
+    <script src="/script.js"></script>
+</head>
